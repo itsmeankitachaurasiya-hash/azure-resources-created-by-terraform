108f9d1b8b947dddce9949a4c0fb759ea928de77
Ankita Chaurasiya <itsmeankitachaurasiya@gmail.com>
2026-06-04 22:23:30 +0530

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -1,9 +1,9 @@
 resource"azurerm_resource_group""resource_block1"{
- name="rg1" 
+  name="rg1" 
   location="eastus"
 }
 resource"azurerm_resource_group""resource_block2"{
- name="rg2" 
+  name="rg2" 
   location="westus"
 }
 
